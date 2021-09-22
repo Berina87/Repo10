cc21634ce04c1ef524c5696e414e79aade03c245
Berina Sanjta <berina@sanjta.org>
2021-09-22 20:53:53 +0200

    Adding folder ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var myPet = {
+    species: "bird",
+    name: "Cvrle",
+    legs: 2,
+    friends: ["Jasmina", "Aida "]
+};
+
+function myFunction(myObj){
+    return myObj;
+}
+
+// Only change code above this line
+
+console.log(myFunction(myPet)); //Change this line
+module.exports = {myPet, myFunction};
